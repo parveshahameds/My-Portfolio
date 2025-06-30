0ebb46f600f2c5fdb00fd34da8145ca3f569163a
Parvesh Ahamed <parveshahamed14@gmail.com>
2025-06-30 12:34:19 +0530

    analyt

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import SkillsPage from "./pages/Skills";
 import Projects from "./pages/Projects";
 import Resume from "./pages/Resume";
@@ -34,6 +35,7 @@ const App = () => (
             </Routes>
           </main>
         </BrowserRouter>
+        <SpeedInsights />
       </div>
     </TooltipProvider>
   </QueryClientProvider>
